a3fd5d6a70a712e3974b9a5f905a822e34371dcf
Andrija25 <93194317+Andrija25@users.noreply.github.com>
2023-06-05 05:56:37 +0200

    bug fix

--- src/main/java/com/vima/accommodation/config/AppConfig.java
+++ src/main/java/com/vima/accommodation/config/AppConfig.java
@@ -4,9 +4,8 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import brave.sampler.Sampler;
+//import brave.sampler.Sampler;
 
-@Configuration
 public class AppConfig {
 
 	@Bean
