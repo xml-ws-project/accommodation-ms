f62d2c40cd3a6e711a1c9994ab0447afc1ca459d
Andrija25 <93194317+Andrija25@users.noreply.github.com>
2023-06-05 05:01:49 +0200

    creating accom node on accommodation create feature added

--- src/main/java/com/vima/accommodation/grpc_service/AccommodationGrpcService.java
+++ src/main/java/com/vima/accommodation/grpc_service/AccommodationGrpcService.java
@@ -1,30 +1,18 @@
 package com.vima.accommodation.grpc_service;
 
+import com.vima.accommodation.dto.gRPCObjectRec;
 import com.vima.accommodation.mapper.AdditionalBenefitMapper;
 import com.vima.accommodation.service.AdditionalBenefitService;
 import com.vima.accommodation.service.SpecialInfoService;
-import com.vima.gateway.BenefitList;
-import com.vima.gateway.DeleteHostAccommodationResponse;
-import com.vima.gateway.DeleteHostAccommodationsRequest;
-import com.vima.gateway.SearchList;
-import com.vima.gateway.SearchRequest;
+import com.vima.gateway.*;
 import com.vima.accommodation.mapper.AccommodationMapper;
 import com.vima.accommodation.mapper.SpecialInfoMapper;
 import com.vima.accommodation.service.AccommodationService;
-import com.vima.gateway.AccommodationList;
-import com.vima.gateway.AccommodationRequest;
-import com.vima.gateway.AccommodationResponse;
-import com.vima.gateway.AccommodationServiceGrpc;
-import com.vima.gateway.AdditionalBenefitRequest;
-import com.vima.gateway.AdditionalBenefitResponse;
-import com.vima.gateway.Empty;
-import com.vima.gateway.SpecialInfoRequest;
-import com.vima.gateway.SpecialInfoResponse;
-import com.vima.gateway.UpdateAccommodationRequest;
-import com.vima.gateway.Uuid;
 
 import java.util.UUID;
 
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 import net.devh.boot.grpc.server.service.GrpcService;
 
 import io.grpc.stub.StreamObserver;
@@ -52,8 +40,14 @@ public class AccommodationGrpcService extends AccommodationServiceGrpc.Accommoda
 	@Override
 	public void create(AccommodationRequest request, StreamObserver<AccommodationResponse> responseObserver) {
 		var accommodation = AccommodationMapper.convertEntityToDto(accommodationService.create(request));
+		createAccomNode(accommodation.getId());
 		responseObserver.onNext(accommodation);
 		responseObserver.onCompleted();
+	}
+
+	private void createAccomNode(String userId){
+		getBlockingStub().getStub().createAccomNode(Uuid.newBuilder().setValue(userId).build());
+		getBlockingStub().getChannel().shutdown();
 	}
 
 	@Override
@@ -125,6 +119,16 @@ public class AccommodationGrpcService extends AccommodationServiceGrpc.Accommoda
 			.build();
 		responseObserver.onNext(response);
 		responseObserver.onCompleted();
+	}
+
+	private gRPCObjectRec getBlockingStub() {
+		ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 9095)
+				.usePlaintext()
+				.build();
+		return gRPCObjectRec.builder()
+				.channel(channel)
+				.stub(RecommendationServiceGrpc.newBlockingStub(channel))
+				.build();
 	}
 
 }
